3fcbd66f2de5f2a571928504952510320ebd8d7d
Harsh Khandelwal <49086820+because-i-m-batman@users.noreply.github.com>
2020-04-21 15:55:16 +0530

    Add files via upload

---
+++ FaceDetection_using_facerecognition_Library.py
@@ -0,0 +1,20 @@
+import numpy as np
+import cv2
+import face_recognition
+import matplotlib.pyplot as plt
+
+image = cv2.imread('TC_1877.png')
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+face_locations = []
+rgb_frame = image[:, :, ::-1]
+
+#Find all the faces in the current frame of video
+face_locations = face_recognition.face_locations(rgb_frame)
+
+for top, right, bottom, left in face_locations:
+    #Draw a box around the face
+    cv2.rectangle(image, (left, top), (right, bottom), (0, 0, 255), 1)
+
+    roi_color = image[top:bottom,left:right]
+    cv2.imwrite('C:\\Users\\Harsh\\OneDrive\\Documents\\Models\\Face Model\\testing_image.png', roi_color)
